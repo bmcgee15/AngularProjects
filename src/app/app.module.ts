c1264e147e75b06b216ff6f41f18aa1209ecea63
Bret McGee <bret.mcgee@mohawkcollege.ca>
2019-01-01 23:40:02 -0500

    Added some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpModule
+    FormsModule
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // most common
+  // selector: '[app-servers]' use if accessing attribute not element
+  // sellector: '.app-servers' use if accessing class name (id does not work)
   templateUrl: './servers.component.html',
+  // template: `<p>html multiline code</p>` one or the other not both
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
